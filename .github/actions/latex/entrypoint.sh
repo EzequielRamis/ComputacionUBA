b961413b1bcee5dd398d0b4622c15a1c9eefa6cf
PeronTheDuck <pedroignacioperezvargas@gmail.com>
2020-05-31 02:41:00 -0300

    Fixed error when entrypoint.sh had to rename the PDFs

--- .github/actions/latex/entrypoint.sh
+++ .github/actions/latex/entrypoint.sh
@@ -2,5 +2,5 @@
 find -name '*.root.tex' | while IFS= read -r file; do
     echo "Found '$file'"
     latexmk -cd -pdf $file
-    mv "${$file%.tex}.pdf" "${$file%.root.tex}.pdf"
+    mv "${file%.tex}.pdf" "${file%.root.tex}.pdf"
 done
