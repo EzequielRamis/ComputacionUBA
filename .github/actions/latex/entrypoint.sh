8666d77fba3803b0f0824fa7f68c9399322c254b
PeronTheDuck <pedroignacioperezvargas@gmail.com>
2020-06-14 23:26:43 -0300

    Actualizada la Action para compilar * Ahora usa LuaLaTeX (-pdflua) * Añadida la opción --file-line-error * Importante! Ahora está activada la opción --shell-escape

--- .github/actions/latex/entrypoint.sh
+++ .github/actions/latex/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 find -name '*.root.tex' | while IFS= read -r file; do
     echo "Found '$file'"
-    latexmk -cd -pdf "$file" &&
+    latexmk -cd -pdflua --file-line-error --shell-escape "$file" &&
         mv "${file%.tex}.pdf" "${file%.root.tex}.pdf"
 done
