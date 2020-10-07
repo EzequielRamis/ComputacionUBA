158e8dc155c89a3dea17447f2cac0acc9baae561
Pedro <pedroignacioperezvargas@gmail.com>
2020-10-06 23:20:01 -0300

    Mejorar Github action
    Builds paralelizados + cache de figuras tikz

--- .github/actions/latex/entrypoint.sh
+++ .github/actions/latex/entrypoint.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
-find -name '*.root.tex' | while IFS= read -r file; do
-    echo "Found '$file'"
-    latexmk -cd -pdflua --file-line-error --shell-escape "$file" &&
-        mv "${file%.tex}.pdf" "${file%.root.tex}.pdf"
-done
+#!/bin/bash
+build() {
+    echo "Compilando $1..."
+    mkdir -p "$(dirname "$1")"/tikz
+    latexmk -cd -pdflua -file-line-error -shell-escape "$1"
+    mv "${1%.tex}.pdf" "${1%.root.tex}.pdf"
+}
+export -f build
+find . -name "*.root.tex" | parallel --halt now,fail=1 build
